d48fab17b7ae9fac50da7b3e5664581f81609d87
Azsrs <farzanehfrank@icloud.com>
2026-01-12 12:59:51 +0100

    Fiender! Nu finns dem!

--- Battleshih/Program.cs
+++ Battleshih/Program.cs
@@ -1,66 +1,94 @@
 ﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using Raylib_cs;
 
-Raylib.InitWindow(800, 800, "Shooty shooty bang bang"); //Skapa window
+Raylib.InitWindow(800, 800, "Place Invaders"); //Create Window
 Raylib.SetTargetFPS(60);
-Raylib.SetExitKey(KeyboardKey.Escape); //Stäng spelet
+Raylib.SetExitKey(KeyboardKey.Escape); //Close Game on Esc.
 float speed = 4; //Movement speed
 
 Texture2D Ship = Raylib.LoadTexture(@"Ship.png"); //Skapa skepp
 Rectangle SHitBox = new(400, 700, Ship.Dimensions);
 
-Texture2D BulletTexture = Raylib.LoadTexture(@"Bullet.png");
+Texture2D BulletTexture = Raylib.LoadTexture(@"Bullet.png"); //Create bullet
 List<Bullet> Bullets = [];
 List<Bullet> toRemove = [];
+
+Texture2D EnemyTexture = Raylib.LoadTexture(@"SpaceInvader.png");
+List<Enemy> Enemies = [];
+
+for (int i = 1; i < 8; i++)
+{
+Enemy enemy = new()
+{
+    Position = new Vector2(i * 90, 80),
+    Alive = true,
+    EnemyRect = new(i * 90, 80, EnemyTexture.Dimensions),
+};
+Enemies.Add(enemy);
+
+}
+
 
 
 while (!Raylib.WindowShouldClose())
 {
-    if (Raylib.IsKeyDown(KeyboardKey.Right)) SHitBox.X += speed;  //Keyboard controls höger
-    if (Raylib.IsKeyDown(KeyboardKey.Left)) SHitBox.X -= speed;  //Keyboard controls vänster
+
+    if (Raylib.IsKeyDown(KeyboardKey.Right)) SHitBox.X += speed;  //Keyboard controls right
+    if (Raylib.IsKeyDown(KeyboardKey.Left)) SHitBox.X -= speed;  //Keyboard controls left
 
 
-    if (SHitBox.X > 750) //Out of bounds stopper höger
+    if (SHitBox.X > 750) //Out of bounds stopper right
     {
         SHitBox.X -= speed;
     }
-    if (SHitBox.X < 0) //Out of bounds stopper vänster
+    if (SHitBox.X < 0) //Out of bounds stopper left
     {
         SHitBox.X += speed;
-    }
+    } 
 
 
-    if (Raylib.IsKeyPressed(KeyboardKey.Space))
+    if (Raylib.IsKeyPressed(KeyboardKey.Space)) //Bullet creation on SPACE-click
     {
-        Bullet b = new()
+        Bullet b = new() //Create bullet after class
         {
-            Velocity = new Vector2(0, -1),
-            Position = new Vector2(SHitBox.X, SHitBox.Y),
+            Velocity = new Vector2(0, -5), 
+            Position = new Vector2(SHitBox.X+27, SHitBox.Y-13), //Spawns at the tip of the ship
+            BulletBox = new(SHitBox.X+27, SHitBox.Y-13, BulletTexture.Dimensions),
         };
-        Bullets.Add(b);
+        Bullets.Add(b); // Add bullets to the list
     }
-
-    
 
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.Black);
+
+
+    foreach (Enemy enemies in Enemies) //Draw enemies to their location 
+    {
     
-foreach (Bullet bullet in Bullets)
+        Raylib.DrawTexture(EnemyTexture, (int)enemies.Position.X, (int)enemies.Position.Y, Color.White);
+        
+    }
+    
+    
+foreach (Bullet bullet in Bullets) //Every bullet in the bullets list moves according to the set velocity
 {
-    bullet.Position += bullet.Velocity;    
-    Raylib.DrawTexture(BulletTexture, (int)bullet.Position.X, (int)bullet.Position.Y, Color.White);
+    bullet.Position += bullet.Velocity;    //Move according to velocity
+    bullet.BulletBox.Position += bullet.Velocity;
+    Raylib.DrawTexture(BulletTexture, (int)bullet.Position.X, (int)bullet.Position.Y, Color.White); //Draw the bullet after changing its position
 }
 
-    Raylib.DrawTexture(Ship, (int)SHitBox.X, (int)SHitBox.Y, Color.White);
+    Raylib.DrawTexture(Ship, (int)SHitBox.X, (int)SHitBox.Y, Color.White); //Draw the Ship/Player
     Raylib.EndDrawing();
 
-
-    foreach (Bullet bullet in Bullets)
+    foreach (Bullet bullet in Bullets) //If a bullet goes offscreen, remove it from the list of bullets
     {
-        if (bullet.Position.Y < 0)
+        if (bullet.Position.Y < 0) 
         {
             toRemove.Add(bullet);
         }
+        
     }
 
     foreach (Bullet bullet in toRemove)
@@ -75,4 +103,14 @@ class Bullet
 {
 public Vector2 Velocity;
 public Vector2 Position;
+public Rectangle BulletBox;
 }
+
+class Enemy
+{
+    public bool Alive;
+    public Vector2 Position;
+
+    public Rectangle EnemyRect;
+}
+
